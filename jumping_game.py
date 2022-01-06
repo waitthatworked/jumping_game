9d3fb5a4a5759bc3f9f724e77ac3a21f42253bca
Wait that Worked? <waitthatworked@gmail.com>
2022-01-06 14:58:42 -0800

    Made the game object oriented

--- jumping_game.py
+++ jumping_game.py
@@ -6,7 +6,7 @@ import pygame
 
 pygame.init()
 
-WIN_HEIGHT = 500
+WIN_HEIGHT = 480
 WIN_WIDTH = 500
 
 win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
@@ -17,6 +17,12 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 white = (255, 255, 255)
+
+# Jumping variables
+ground = WIN_HEIGHT - 80
+gravity = 0.5
+timeDelay = 50 #milliseconds
+timer = 0
 
 # Player sprites
 walkRight = [pygame.image.load('sprites/R1.png'), 
@@ -36,39 +42,97 @@ char = pygame.image.load('sprites/standing.png')
 
 pygame.display.set_caption("Jumping Game")
 
-x = 50
-y = 50
-width = 40
-height = 60
-vel = 15
+# Variables
+clock = pygame.time.Clock()
 
-# Jumping variables
+class player(object):
+    def __init__(self, x, y, width, height):
+        
+        # Movement and position variables
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 5
+        self.walkCount = 0
+        self.left = False
+        self.right = False
 
-gravity = 0.5
-isJump = False
-jumpCount = 10
-timeDelay = 50 #milliseconds
-touchingSurface = True
-timer = 0
+        # Jumping Variables
+        self.isJump = False
+        self.touchingSurface = True
+        self.jumpTimer = 0
 
+    def draw(self, win):
+        if self.walkCount + 1 >= 27:
+            self.walkCount = 0
+
+        if self.left:
+            win.blit(walkLeft[self.walkCount//3], (self.x, self.y))
+            self.walkCount += 1
+        elif self.right:
+            win.blit(walkRight[self.walkCount//3], (self.x, self.y))
+            self.walkCount += 1
+        else:
+            win.blit(char, (self.x, self.y))
+            self.walkCount += 1
+
+# Redraws the game window when called
+def redrawGameWindow():
+    win.blit(bg, (0, 0))
+    worm.draw(win)
+    pygame.display.update()
+
+# Main Loop:
+worm = player(WIN_WIDTH/3, ground, 64, 64)
 run = True
 while run:
+    clock.tick(27)
     pygame.time.delay(timeDelay)
-
-    # if y < WIN_HEIGHT - height:
-    #     y += gravity
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] and x > 0:
-        x -= vel
-    if keys[pygame.K_RIGHT] and x < WIN_WIDTH - width:
-        x += vel
+    if keys[pygame.K_LEFT] and worm.x > 0:
+        worm.x -= worm.vel
+        worm.left = True
+        worm.right = False
+    elif keys[pygame.K_RIGHT] and worm.x < WIN_WIDTH - worm.width:
+        worm.x += worm.vel
+        worm.left = False
+        worm.right = True
+    else:
+        worm.right = False
+        worm.left = False
+        worm.walkCount = 0
+
+    # My jumping code:
+    if not(worm.isJump):
+        if keys[pygame.K_SPACE] and worm.touchingSurface:
+            worm.isJump = True
+            worm.right = False
+            worm.left = False
+            worm.walkCount = 0
+            
+            worm.touchingSurface = False
+            worm.jumpTimer = 0
+    else:
+        # v = u + at
+        vel_y = 100 - (gravity * (timeDelay * worm.jumpTimer))
+        worm.y -= vel_y
+        worm.jumpTimer += 1
+        
+        if worm.y >= ground:
+            worm.y = ground
+            worm.isJump = False
+            worm.touchingSurface = True
+    
+    redrawGameWindow()
+
+pygame.quit()
 
     # Tech with Tim's Strange Jumping Code:    
     # if not(isJump):
@@ -88,32 +152,3 @@ while run:
     #     else:
     #         isJump = False
     #         jumpCount = 10
-
-    # My jumping code:
-    if not(isJump):
-        if keys[pygame.K_UP] and y > 0:
-            y -= vel
-        if keys[pygame.K_DOWN] and y < WIN_HEIGHT - height:
-            y += vel
-        if keys[pygame.K_SPACE] and touchingSurface:
-            isJump = True
-            vel_y = 20
-            touchingSurface = False
-            timer = 0
-
-    else:
-        vel_y = 100 - (gravity * (timeDelay * timer))
-        y -= vel_y
-        timer += 1
-        
-        if y > WIN_HEIGHT - 2*height:
-            isJump = False
-            touchingSurface = True
-            y = WIN_HEIGHT - height
-
-    win.fill(black)
-
-    pygame.draw.rect(win, red, (x, y, width, height))
-    pygame.display.update()
-
-pygame.quit()
